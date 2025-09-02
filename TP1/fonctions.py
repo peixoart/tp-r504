748eee7bf9d8ce66233e9c637cf29bc68a179e64
peixoart <arthur.peixoto18@outlook.fr>
2025-09-02 09:56:52 -0400

    Test Partie 2 TP1

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,13 +1,9 @@
-def puissance():
-    a = input("Choisissez la valeur a : ")
-    b = input("Choisissez la valeur b : ")
-
-    if not a.lstrip('-').isdigit():
+def puissance(a,b):	
+  
+    if not type(a) is int :
         raise TypeError("Only integers are allowed for a")
-    if not b.lstrip('-').isdigit():
+    if not type(b) is int :
         raise TypeError("Only integers are allowed for b")
+    
+    return a ** b
 
-    resultat = a ** b
-    print(f"Le résultat de {a} à la puissance {b} est égal à {resultat}\n")
-
-puissance()
--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,6 +1,9 @@
 import fonctions as f
 
 while True : 
+	a = int(input("Choisissez la valeur a : "))
+        b = int(input("Choisissez la valeur b : "))
 	nombre = int(input("Veuillez écrire une valeur : "))
-	carre = f.puissance(a,b)
-	print(f"Le carré de {nombre} est {carre}\n")
+	resultat = f.puissance(a,b)
+	print(f"Le carré de {nombre} est {resultat}\n")
+
