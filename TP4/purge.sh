efb9efdceebd47af088277f75793c11908622807
peixoart <arthur.peixoto18@outlook.fr>
2025-09-10 09:46:18 +0200

    modified

--- TP4/purge.sh
+++ TP4/purge.sh
@@ -1,2 +1,5 @@
-docker stop(docker ps-aq)
-docker rm(docker ps -aq)
+docker stop $(docker ps -aq) 
+docker rm $(docker ps -aq) 
+docker rmi $(docker images -aq) 
+docker volume prune -f
+docker network prune -f
