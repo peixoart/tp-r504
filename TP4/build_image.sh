5a3e76529b6245189dd31f49e687605dc8db9cd9
peixoart <arthur.peixoto18@outlook.fr>
2025-09-11 16:00:45 +0200

    update

--- TP4/build_image.sh
+++ TP4/build_image.sh
@@ -1,1 +1,1 @@
-docker build -t im-tp4 -f Dockerfile1
+docker build -t im-tp4 -f /home/fa25-peixo/tp-r504/TP4/Dockerfile1 .
---
+++ TP4/run_app.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker run -d \
+  --name tp4-app \
+  --network net-tp4 \
+  -p 5000:5000 \
+  im-tp4
+
